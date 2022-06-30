fe55d70a17957ffd0122f15f938a9aaa7ecf826d
Laicasaane <laicasaane@outlook.com>
2022-06-30 18:50:51 +0700

    Fixed size buffer example

--- Assets/Scripts/DemoRuntime.cs
+++ Assets/Scripts/DemoRuntime.cs
@@ -7,5 +7,66 @@ namespace Game.Runtime
 {
     public class DemoRuntime : MonoBehaviour
     {
+        private void Start()
+        {
+            var x = new FixedBuffer_4<string>();
+
+            for (var i = 0; i < x.Length; i++)
+                x.SetUnsafe(i, i.ToString());
+
+            for (var i = 0; i < x.Length; i++)
+                Debug.Log(x.GetUnsafe(i));
+
+        }
+    }
+
+    [UnsafeValueType, StructLayout(LayoutKind.Sequential, Pack = 0)]
+    public struct FixedBuffer_4<T>
+    {
+        private T _e0;
+        private T _e1;
+        private T _e2;
+        private T _e3;
+
+        public const int LENGTH = 4;
+
+        public int Length
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => LENGTH;
+        }
+
+        public T this[int index]
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get
+            {
+                if ((uint)index >= LENGTH)
+                    throw new IndexOutOfRangeException();
+
+                return Unsafe.Add<T>(ref _e0, index);
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            set
+            {
+                if ((uint)index >= LENGTH)
+                    throw new IndexOutOfRangeException();
+
+                Unsafe.Add<T>(ref _e0, index) = value;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public T GetUnsafe(int index)
+        {
+            return Unsafe.Add<T>(ref _e0, index);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public unsafe void SetUnsafe(int index, T value)
+        {
+            Unsafe.Add<T>(ref _e0, index) = value;
+        }
     }
 }
