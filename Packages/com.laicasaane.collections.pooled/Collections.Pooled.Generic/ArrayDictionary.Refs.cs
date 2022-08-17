4fed0ddc4b66c2ee79cd92aca502a024e6f7ce1e
Laicasaane <laicasaane@outlook.com>
2022-08-17 16:17:22 +0700

    Expose internal methods of ArrayDictionary since they are safe

--- Packages/com.laicasaane.collections.pooled/Collections.Pooled.Generic/ArrayDictionary.Refs.cs
+++ Packages/com.laicasaane.collections.pooled/Collections.Pooled.Generic/ArrayDictionary.Refs.cs
@@ -6,7 +6,7 @@ namespace Collections.Pooled.Generic
     partial class ArrayDictionary<TKey, TValue>
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal ref TValue GetOrAdd(TKey key)
+        public ref TValue GetOrAdd(TKey key)
         {
             if (TryFindIndex(key, out var findIndex) == true)
             {
@@ -21,7 +21,7 @@ namespace Collections.Pooled.Generic
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal ref TValue GetOrAdd(in TKey key)
+        public ref TValue GetOrAdd(in TKey key)
         {
             if (TryFindIndex(in key, out var findIndex) == true)
             {
@@ -36,7 +36,7 @@ namespace Collections.Pooled.Generic
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal ref TValue GetOrAdd(TKey key, Func<TValue> builder)
+        public ref TValue GetOrAdd(TKey key, Func<TValue> builder)
         {
             if (TryFindIndex(key, out var findIndex) == true)
             {
@@ -51,7 +51,7 @@ namespace Collections.Pooled.Generic
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal ref TValue GetOrAdd(in TKey key, Func<TValue> builder)
+        public ref TValue GetOrAdd(in TKey key, Func<TValue> builder)
         {
             if (TryFindIndex(in key, out var findIndex) == true)
             {
@@ -66,7 +66,7 @@ namespace Collections.Pooled.Generic
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal ref TValue GetOrAdd<W>(TKey key, FuncRef<W, TValue> builder, ref W parameter)
+        public ref TValue GetOrAdd<W>(TKey key, FuncRef<W, TValue> builder, ref W parameter)
         {
             if (TryFindIndex(key, out var findIndex) == true)
             {
@@ -81,7 +81,7 @@ namespace Collections.Pooled.Generic
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal ref TValue GetOrAdd<W>(in TKey key, FuncRef<W, TValue> builder, ref W parameter)
+        public ref TValue GetOrAdd<W>(in TKey key, FuncRef<W, TValue> builder, ref W parameter)
         {
             if (TryFindIndex(in key, out var findIndex) == true)
             {
@@ -96,7 +96,7 @@ namespace Collections.Pooled.Generic
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal ref TValue RecycleOrAdd<TValueProxy>(TKey key, Func<TValueProxy> builder, ActionRef<TValueProxy> recycler)
+        public ref TValue RecycleOrAdd<TValueProxy>(TKey key, Func<TValueProxy> builder, ActionRef<TValueProxy> recycler)
             where TValueProxy : class, TValue
         {
             if (TryFindIndex(key, out var findIndex) == true)
@@ -115,7 +115,7 @@ namespace Collections.Pooled.Generic
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal ref TValue RecycleOrAdd<TValueProxy>(in TKey key, Func<TValueProxy> builder, ActionRef<TValueProxy> recycler)
+        public ref TValue RecycleOrAdd<TValueProxy>(in TKey key, Func<TValueProxy> builder, ActionRef<TValueProxy> recycler)
             where TValueProxy : class, TValue
         {
             if (TryFindIndex(in key, out var findIndex) == true)
@@ -146,7 +146,7 @@ namespace Collections.Pooled.Generic
         /// <typeparam name="U"></typeparam>
         /// <returns></returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal ref TValue RecycleOrAdd<TValueProxy, U>(TKey key
+        public ref TValue RecycleOrAdd<TValueProxy, U>(TKey key
             , FuncRef<U, TValue> builder
             , ActionRef<TValueProxy, U> recycler
             , ref U parameter
@@ -181,7 +181,7 @@ namespace Collections.Pooled.Generic
         /// <typeparam name="U"></typeparam>
         /// <returns></returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal ref TValue RecycleOrAdd<TValueProxy, U>(in TKey key
+        public ref TValue RecycleOrAdd<TValueProxy, U>(in TKey key
             , FuncRef<U, TValue> builder
             , ActionRef<TValueProxy, U> recycler
             , ref U parameter
@@ -206,13 +206,13 @@ namespace Collections.Pooled.Generic
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         //WARNING this method must stay stateless (not relying on states that can change, it's ok to read 
         //constant states) because it will be used in multi-threaded parallel code
-        internal ref TValue GetIndexedValueByRef(int index)
+        public ref TValue GetIndexedValueByRef(int index)
         {
             return ref _values[index];
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal ref TValue GetValueByRef(TKey key)
+        public ref TValue GetValueByRef(TKey key)
         {
 #if DEBUG
             if (TryFindIndex(key, out var findIndex) == true)
@@ -229,7 +229,7 @@ namespace Collections.Pooled.Generic
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal ref TValue GetValueByRef(in TKey key)
+        public ref TValue GetValueByRef(in TKey key)
         {
 #if DEBUG
             if (TryFindIndex(in key, out var findIndex) == true)
